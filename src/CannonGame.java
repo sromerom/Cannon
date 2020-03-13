481d64085bfc05182e39bdc2efa3be4e957abb94
Samuel Romero <sromerom@esliceu.net>
2020-03-13 18:34:36 +0100

    Creada estructura basica

---
+++ src/CannonGame.java
@@ -0,0 +1,25 @@
+import org.newdawn.slick.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CannonGame extends BasicGame {
+    public CannonGame(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            CannonGame cg = new CannonGame("Provant");
+            AppGameContainer app = new AppGameContainer(cg);
+            app.setTargetFrameRate(60);
+            app.setShowFPS(true);
+            app.setDisplayMode(800, 600, false);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
